b3c2ba303e53aabae7acc32894ec108c8c922740
mortale <1695588928@qq.com>
2022-04-21 00:29:41 +0800

    debug deploy

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,11 @@ cd docs/.vitepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'theme deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
- git push -f git@github.com:mortale/mortale.github.io.git master
+ git push -f https://mortale:${GITHUB_TOKEN}git@github.com:mortale/mortale.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
