f52215fb7bed049a60b1c3aa1a4184cbba56e193
mortale <1695588928@qq.com>
2022-04-21 16:41:36 +0800

    debug deploy

--- deploy.sh
+++ deploy.sh
@@ -8,21 +8,20 @@ npm run docs:build
 
 # 进入待发布的目录
 cd docs/.vitepress/dist
-echo 111
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
-# git config --global user.email ${EMAIL}
-# echo 1
-# git config --global user.name mortale
-# echo 2
-# git add -A
-# echo 3
-# git commit -m 'theme deploy'
-# echo 4
+git config --global user.email ${EMAIL}
+echo 1
+git config --global user.name mortale
+echo 2
+git add -A
+echo 3
+git commit -m 'theme deploy'
+echo 4
 
 # 如果部署到 https://<USERNAME>.github.io
-# git push -f https://git@github.com:mortale/mortale.github.io.git master
-# echo 5
+git push -f https://git@github.com:mortale/mortale.github.io.git master
+echo 5
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
