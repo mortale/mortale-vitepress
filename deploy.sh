461c3dabc80e9a586f235c5e5fa6163afec2ce71
mortale <1695588928@qq.com>
2022-04-21 16:11:06 +0800

    debug deploy

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,25 @@
 #!/usr/bin/env sh
 
 # 忽略错误
-# set -e
+set -e
 
 # 构建
-# npm run docs:build
+npm run docs:build
 
 # 进入待发布的目录
-# cd docs/.vitepress/dist
+cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
-echo 1111
-echo ${EMAIL}
-# git config --global user.email ${EMAIL}
-# git config --global user.name mortale
-# git init
-# git add -A
-# git commit -m 'theme deploy'
+git config --global user.email ${EMAIL}
+git config --global user.name mortale
+git add -A
+git commit -m 'theme deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
-# git push -f https://git@github.com:mortale/mortale.github.io.git master
+git push -f https://git@github.com:mortale/mortale.github.io.git master
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-# cd -
+cd -
