ff533ea6b6b625a3f4150536059ca2a8f559a603
mortale <1695588928@qq.com>
2022-04-26 16:26:22 +0800

    add favicon

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,12 @@
 module.exports = {
     title: 'Hello VitePress',
     description: 'Just playing around.',
+    base:"./",
+    head:[
+        ['link',
+        {rel:'icon',href:"/favicon.ico"}
+    ]
+    ],
         // 主题配置
         themeConfig: {
           //   头部导航
