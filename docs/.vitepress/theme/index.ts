017905faab1634306ab4d65a87de0a85e973bb5c
1695588928@qq.com <1695588928@qq.com>
2022-06-20 23:45:35 +0800

    update micro-app.start

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,7 +4,44 @@ import { MicroApp } from "../../../view/micro-app";
 
 import './style.less'
 
-microApp.start();
+microApp.start({
+   /**
+   * 自定义fetch
+   * @param url 静态资源地址
+   * @param options fetch请求配置项
+   * @returns Promise<string>
+   */
+    fetch(url: string, options: any, appName: string|null) {
+  
+      // if (url === 'http://localhost:3001/error.js') {
+      //   return Promise.resolve('')
+      // }
+  
+        let config:RequestInit | null = {}
+      //  if(/.js$/.test(url)){
+      //   config= {
+      //     headers: {
+      //       "Content-Type":'application/x-javascript',
+      //     },
+      //   }
+      //  }else if(/.css$/.test(url)) {
+      //   config= {
+      //     headers: {
+      //       "Content-Type":'text/css'
+      //     }
+      //   }
+      //  }
+
+      return fetch(url, Object.assign(options, config)).then((res) => {
+        if(/.js$/.test(url)){
+        res.headers['Content-Type'] = 'application/x-javascript'
+         }else if(/.css$/.test(url)) {
+          res.headers['Content-Type'] = 'text/css'
+         }
+        return res.text()
+      })
+    }
+});
 
 export default {
   ...Theme,
